d23fae095452fc2ae67e831adf22e48ed0f430b5
Abenezer <abenezerkifle000@gmail.com>
2024-08-04 00:26:24 -0700

    update next config

--- next.config.js
+++ next.config.js
@@ -1,18 +1,4 @@
-const nextConfig = {
-    async headers() {
-      return [
-        {
-          source: '/(.*)',
-          headers: [
-            {
-              key: 'Content-Security-Policy',
-              value: "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' 'wasm-unsafe-eval'; style-src 'self'; img-src 'self' data:; font-src 'self'; connect-src 'self' https://vercel.live; object-src 'none'; base-uri 'self';",
-            },
-          ],
-        },
-      ];
-    },
-  };
-  
-  module.exports = nextConfig;
-  
+/** @type {import('next').NextConfig} */
+const nextConfig = {}
+
+module.exports = nextConfig
